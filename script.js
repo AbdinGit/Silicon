011d5a89dbb1479abf86d7b0a386ca1a6cd1c4ed
Kicker <Kicker@ABDINDATOR>
2024-03-05 20:40:05 +0100

    teste

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var questions = document.querySelectorAll('.questions');
+  var questions = document.querySelectorAll('.questions');
 
-    questions.forEach(function(question) {
+  questions.forEach(function(question) {
       var moreElement = question.querySelector('.more');
       var iconElement = moreElement.querySelector('i');
 
       moreElement.addEventListener('click', function() {
-        questions.forEach(function(q) {
-          if (q !== question) {
-            q.classList.remove('expanded');
-            q.querySelector('i').classList.remove('fa-chevron-up');
-            q.querySelector('i').classList.add('fa-chevron-down');
-          }
-        });
-        
-        question.classList.toggle('expanded');
-        iconElement.classList.toggle('fa-chevron-down');
-        iconElement.classList.toggle('fa-chevron-up');
+          questions.forEach(function(q) {
+              if (q !== question) {
+                  q.classList.remove('expanded');
+                  q.querySelector('i').classList.remove('fa-chevron-up');
+                  q.querySelector('i').classList.add('fa-chevron-down');
+              }
+          });
+
+          question.classList.toggle('expanded');
+          iconElement.classList.toggle('fa-chevron-down');
+          iconElement.classList.toggle('fa-chevron-up');
       });
-    });
   });
-  document.addEventListener('DOMContentLoaded', function() {
-    var returnHomeButton = document.getElementById("error-btn-return-home");
-    returnHomeButton.addEventListener("click", function() {
+
+  var returnHomeButton = document.getElementById("error-btn-return-home");
+  returnHomeButton.addEventListener("click", function() {
       window.location.href = "home.html";
-    });
+  });
+  
+  var returnHomeButton = document.getElementById("appointmentbtn");
+  returnHomeButton.addEventListener("click", function() {
+      window.location.href = "home.html";
+  });
 });
